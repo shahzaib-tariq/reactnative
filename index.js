d9ab7eca06d12c5221a7d8052871d01368994f02
unknown <shozych@gmail.com>
2018-05-18 10:25:38 +0500

    secong commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Header from './src/component/header';
 const App=()=>{
 
-    return<Header headertext={'albumsnew12'} />;
+    return<Header headertext={'albumsnew123'} />;
 };
 
 AppRegistry.registerComponent('albums', () => App);
