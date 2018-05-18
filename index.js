a08daaba50d92133d0712bdb684b4efb0489c1eb
unknown <shozych@gmail.com>
2018-05-18 11:22:41 +0500

    five commit

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-import { Text,AppRegistry } from 'react-native';
+import {AppRegistry,View } from 'react-native';
 import React from 'react';
 import Header from './src/component/header';
+import AlbumList from  './src/component/AlbumList';
+
 const App=()=>{
 
-    return<Header headertext={'albumsnew123'} />;
+  <View>
+    <Header headertext={'albumsnew123'} />;
+<AlbumList/>
+  </View>
 };
 
 AppRegistry.registerComponent('albums', () => App);
