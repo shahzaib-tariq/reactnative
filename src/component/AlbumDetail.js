26ea04393cfe079d2b5991c7d52d04130cacb974
unknown <shozych@gmail.com>
2018-05-20 16:54:01 +0500

    sunday

---
+++ src/component/AlbumDetail.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+import { View,Text } from 'react-native';
+
+const AlbumDetail = (props) => {
+    return (
+        <View>
+        <Text >{props.album.title}</Text>
+    </View>
+    );
+
+};
+
+export default AlbumDetail;
